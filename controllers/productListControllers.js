6edc570a3a6a84a69fc37ffca863a3d32681f159
Wang Lei <wldandan@gmail.com>
2014-08-10 17:02:14 +0800

    highlight selected category

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -1,5 +1,6 @@
 angular.module("sportsStore")
-    .controller("productListCtrl", function ($scope, $filter) {
+    .constant("productListActiveClass", "btn-primary")
+    .controller("productListCtrl", function ($scope, $filter, productListActiveClass) {
 
         var selectedCategory = null;
 
@@ -10,5 +11,9 @@ angular.module("sportsStore")
         $scope.categoryFilterFn = function (product) {
             return selectedCategory == null ||
                 product.category == selectedCategory;
+        }
+
+        $scope.getCategoryClass = function(category){
+          return selectedCategory == category ? productListActiveClass : "";
         }
     });
